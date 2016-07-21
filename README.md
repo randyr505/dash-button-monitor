c38d5c3b735d2dfc504f91b4e19b72a4a3789a84
Randy Rasmussen <randyr505@iMac.local>
2016-07-20 19:00:46 -0500

    modified:   README.md 	new file:   dash_monitor.py 	new file:   dash_monitor.startup

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# dash-button-monitor
+# monitor-dash-button
 Displays mac addresses of dash buttons. Runs on a raspberry pi
+
+I am not sure who all contributed to the arp parsing code since it was posted on medium via anonymous but I'll provide the link here. 
+
+Medium post: 
+  https://medium.com/@xtalker/hey-ted-i-got-this-working-on-my-raspi-without-all-the-scapy-overhead-thanks-to-http-e5704e4b16a9#.18n4jw26u
+
+Original code I modified: 
+  https://gist.github.com/anonymous/2d6eb47a0d531c20ed54
+
+I added code to loop through multiple macs and print to stdout. Additionally to omit certain macs
+
+# Run dash_monitor to discover mac addresses
+sudo python dash_monitor.py
+
+# Start dash_monitor as a service
+sudo cp dash_monitor.startup /etc/init.d/dash_monitor
+sudo update-rc.d dash_monitor defaults
---
+++ dash_monitor.py
@@ -0,0 +1,29 @@
+import socket
+import struct
+import binascii
+rawSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
+while True:
+  packet = rawSocket.recvfrom(2048)
+  ethernet_header = packet[0][0:14]
+  ethernet_detailed = struct.unpack("!6s6s2s", ethernet_header)
+  arp_header = packet[0][14:42]
+  arp_detailed = struct.unpack("2s2s1s1s2s6s4s6s4s", arp_header)
+  ethertype = ethernet_detailed[2] 
+  # skip non-ARP packets
+  if ethertype != '\x08\x06':
+    continue
+  matched_source = False
+  source_mac = binascii.hexlify(arp_detailed[5])
+  dest_ip = socket.inet_ntoa(arp_detailed[8])
+
+  exclude_macs = ('801f01000000', '801f02000000')
+  dashes = [["74c2460000", "Milk"], ["74c246000000", "Stacys"], ["00bb3a000000", "Downy"], ["44650d000000", "Red Bull"], ["747548000000", "Mucinex"], ["74c246000000", "Gatorade"], ["f0272d000000", "Energizer"]]
+
+  for (dash_mac,dash_name) in dashes:
+    if source_mac == dash_mac:
+      print "~~~ " + dash_name + " button pressed, IP = " + dest_ip + " and mac = " + source_mac
+      matched_source = True
+      
+  if matched_source == False:
+    if source_mac not in exclude_macs:
+      print "Other arp request, IP = " + dest_ip + " and mac = " + source_mac
